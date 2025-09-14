4e7ce69e16b5cf84dbba3e7ed61fa603a3fe5d28
setupcoco-debug <setupcoco@gmail.com>
2025-09-14 14:12:42 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>API OTP thomas</h1>
+      <p>Routes: <code>POST /api/otp/request</code> et <code>POST /api/otp/verify</code></p>
+    </main>
+  );
+}
