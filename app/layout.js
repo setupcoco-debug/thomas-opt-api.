b51a95c06be006df84bd3b08d8cff180492f6da3
setupcoco-debug <setupcoco@gmail.com>
2025-09-14 14:11:59 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,8 @@
+export const metadata = { title: "thomas • API OTP" };
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body style={{ padding: 20, fontFamily: "system-ui" }}>{children}</body>
+    </html>
+  );
+}
